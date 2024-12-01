cdd35c481c8fa92013f6cb0c2bb97cdce190512a
Pavan Prasath V <100416549+pavanjack7@users.noreply.github.com>
2024-12-02 01:17:28 +0530

    Update TicTacToe.cpp

--- tic tac toe/TicTacToe.cpp
+++ tic tac toe/TicTacToe.cpp
@@ -1,153 +1,99 @@
 #include <iostream>
-#include <conio.h>
-#include <Windows.h>
+#include <stdlib.h>
+using namespace std;
+#define R_COUNT 3
+char board[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+int r, c = 0;
+char turn = 'X';
+bool draw = false;
 
-int g_snakeX, g_snakeY, g_fruitX, g_fruitY, g_score;
-int iTailX[100], iTailY[100];
-int g_Width = 20;
-int g_Height = 17;
-bool g_GameOver = false;
-int nTail;
-enum eDirection {STOP =0, RIGHT, LEFT, UP, DOWN } eDir;
-
-void Setup() {
-    eDir = STOP;
-    g_score = 0;
-    g_snakeX = g_Width / 2;
-    g_snakeY = g_Height / 2;
-    g_fruitX = rand() % g_Width;
-    g_fruitY = rand() % g_Height;
-}
-
-void Draw()
-{
+void display_board() {
     system("cls");
-    for (int index = 0; index < g_Width + 2; index++) {
-        std::cout << "#";
+    // cout << "Player1 [X]\ Player2 [O] turn\n";
+    cout << "\t\t    |     |    \n";
+    cout << "\t\t " << board[0][0] << "   |   " << board[0][1] << "  |   " << board[0][2] << endl;
+    cout << "\t\t____|_____|____\n";
+    cout << "\t\t    |     |    \n";
+    cout << "\t\t " << board[1][0] << "   |   " << board[1][1] << "  |   " << board[1][2] << endl;
+    cout << "\t\t____|_____|____\n";
+    cout << "\t\t    |     |    \n";
+    cout << "\t\t " << board[2][0] << "   |   " << board[2][1] << "   |   " << board[2][2] << endl;
+    cout << "\t\t    |     |    \n";
+}
+
+
+bool gameover() {
+    for (int r = 0;r < R_COUNT;r++) {
+        if ((board[r][0] == board[r][1] == board[r][2]) ||
+            (board[0][r] == board[1][r] == board[2][r])) {
+            return false;
+        }
+        else if ((board[0][0] == board[1][1] == board[2][2]) ||
+            (board[0][2] == board[1][1] == board[2][0])) {
+            return false;
+        }
     }
-    std::cout << std::endl;
-    for (int row = 0; row < g_Height; row++) {
-        for (int column = 0; column < g_Width; column++) {
-            if (column == 0)
-                std::cout << "#";
-            if (column == g_snakeX && row == g_snakeY)
-                std::cout << "*";
-            else if (column == g_fruitX && row == g_fruitY)
-                std::cout << "%";
-            else
-            {
-                bool bPrintTail = false;
-                for (int index = 0; index < nTail; index++) {
-                    if (iTailX[index] == column && iTailY[index] == row) {
-                        std::cout << "*";
-                        bPrintTail = true;
-                    }
-                }
-                if (!bPrintTail)
-                    std::cout << " ";
+    for (int r = 0;r < R_COUNT;r++) {
+        for (int c = 0;c < R_COUNT;c++) {
+            if (board[r][c] != 'X' && board[r][c] != 'O') {
+                return true;
             }
-            if (column == g_Width - 1)
-                std::cout << "#";
-        }
-        std::cout << std::endl;
-    }
-    for (int bIndex = 0; bIndex < g_Width + 2; bIndex++) {
-        std::cout << "#";
-    }
-    std::cout << std::endl;
-    std::cout << "Score :" << g_score << std::endl;
-}
-
-void Input() {
-    if (_kbhit()) {
-        switch (_getch())
-        {
-        case 'a':
-            eDir = LEFT;
-            break;
-        case 'w':
-            eDir = UP;
-            break;
-        case 's':
-            eDir = DOWN;
-            break;
-        case 'd':
-            eDir = RIGHT;
-            break;
-        case 'x':
-            g_GameOver = true;
-            break;
-        default:
-            break;
         }
     }
+    draw = true;
+    return false;
 }
 
-void algorithm() {
-    Sleep(150);
-    int prevX, prevY, prev2X, prev2Y;
-    prevX = iTailX[0];
-    prevY = iTailY[0];
-    iTailX[0] = g_snakeX;
-    iTailY[0] = g_snakeY;
-    for (int index = 1; index < nTail; index++) {
-        prev2X = iTailX[index];
-        prev2Y = iTailY[index];
-        iTailX[index] = prevX;
-        iTailY[index] = prevY;
-        prevX = prev2X;
-        prevY = prev2Y;
+void player_turn() {
+    if (turn == 'X') {
+        cout << "\n\t player1  X turn:";
     }
-
-    switch (eDir) {
-    case LEFT:
-        g_snakeX--;
-        break;
-    case RIGHT:
-        g_snakeX++;
-        break;
-    case UP: 
-        g_snakeY--;
-        break;
-    case DOWN:
-        g_snakeY++;
-        break;
+    else if (turn == 'O') {
+        cout << "\n\t player2 O turn:";
+        int choice = 0;
+    cin >> choice;
+    switch (choice) {
+    case 1: r = 0; c = 0; break;
+    case 2: r = 0; c = 1; break;
+    case 3: r = 0; c = 2; break;
+    case 4: r = 1; c = 0; break;
+    case 5: r = 1; c = 1; break;
+    case 6: r = 1; c = 2; break;
+    case 7: r = 2; c = 0; break;
+    case 8: r = 2; c = 1; break;
+    case 9: r = 2; c = 2; break;
     }
-    
-    //Handle out of boundary cases
-    if (g_snakeX > g_Width)
-        g_snakeX = 0;
-    else if (g_snakeX < 0)
-        g_snakeX = g_Width - 1;
-    if (g_snakeY > g_Height)
-        g_snakeY = 0;
-    else if (g_snakeY < 0)
-        g_snakeY = g_Height - 1;
-
-    for (int index = 0; index < nTail; index++) {
-        if (iTailX[index] == g_snakeX && iTailY[index] == g_snakeY)
-            g_GameOver = true;
+    if (turn == 'X' && board[r][c] != 'X' && board[r][c] != 'O') {
+        board[r][c] = 'X';
+        turn = 'O';
     }
-
-    if (g_snakeX == g_fruitX && g_snakeY == g_fruitY) {
-        g_score += 10;
-        g_fruitX = rand() % g_Width;
-        g_fruitY = rand() % g_Height;
-        nTail++;
+    else if (turn == 'O' && board[r][c] != 'X' && board[r][c] != 'O') {
+        board[r][c] = 'O';
+        turn = 'X';
     }
-
-
+    else {
+        cout << "Box already filled \n\n";
+        player_turn();
+    }
+   
 }
 
+int main() {
+        while (gameover()) {
+            display_board();
+            player_turn();
+            gameover();
+        }
 
-int main()
-{
-    std::cout << "Welcome to Snake Game\n";
-    Setup();
-    while (!g_GameOver) {
-        Draw();
-        Input();
-        algorithm();
+        if (turn == 'X' && draw == false) {
+            cout << "\n\t Player 1 won \t\n";
+        }
+        else if (turn == 'O' && draw == false) {
+            cout << "\n\t P2 won \t\n";
+        }
+        else {
+            cout << "\n\t Game ended in DRAW!!\n\t";
+        }
+        return 0;
     }
-    return 0;
 }
